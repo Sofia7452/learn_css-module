7f07a5fb201f383c61103f1f23e4101c15b3ff19
Sofia <sofia7452@163.com>
2020-03-07 17:16:27 +0800

    第一次提交文件

--- demo01/components/App.js
+++ demo01/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import style from './App.css';
-
-export default () => {
+import Demo from './Demo.js'
+export default App=() => {
   return (
     <h1 className={style.title}>
+      <Demo/>
       Hello World
     </h1>
   );
---
+++ demo01/components/Demo.js
@@ -0,0 +1,5 @@
+export default Demo = () => {
+  return (
+    <div className={styles.demo}>demo</div>
+  )
+}
